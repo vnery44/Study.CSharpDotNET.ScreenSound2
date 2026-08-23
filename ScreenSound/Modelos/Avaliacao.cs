b561d900b9e36c94a3adfb99a2935ef4df376a67
Vinicius Nery <vinicius_nery2@hotmail.com>
2026-08-23 15:08:34 -0300

    criado um metodo estatico na classe avaliacao para transformar todo texto que receber em um objeto avaliacao

--- ScreenSound/Modelos/Avaliacao.cs
+++ ScreenSound/Modelos/Avaliacao.cs
@@ -7,4 +7,10 @@ internal class Avaliacao
     {
         Nota = nota;
     }
+
+    public static Avaliacao Parse(string texto)
+    {
+        int nota = int.Parse(texto);
+        return new Avaliacao(nota);
+    }
 }
