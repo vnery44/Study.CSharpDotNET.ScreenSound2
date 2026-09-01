e6d27a933aa54b31202e5629a387ea24f6355f24
Vinicius Nery <vinicius_nery2@hotmail.com>
2026-09-01 20:10:30 -0300

    Implementado IAvaliavel na classe Album, AdicionarNota e Media foram implementados na classe Album, criado MenuAvaliarAlbum herdando Menu

---
+++ ScreenSound/Menus/MenuAvaliarAlbum.cs
@@ -0,0 +1,38 @@
+﻿using ScreenSound.Modelos;
+
+namespace ScreenSound.Menus;
+
+internal class MenuAvaliarAlbum : Menu
+{
+    public override void Executar(Dictionary<string, Banda> bandasRegistradas)
+    {
+        base.Executar(bandasRegistradas);
+
+        ExibirTituloDaOpcao("Avaliar álbum");
+        Console.Write("Digite o nome da banda que deseja avaliar: ");
+        string nomeDaBanda = Console.ReadLine()!;
+        if (bandasRegistradas.ContainsKey(nomeDaBanda))
+        {
+            Banda banda = bandasRegistradas[nomeDaBanda];
+            Console.Write("Agora digite o título do álbum: ");
+            string tituloAlbum = Console.ReadLine()!;
+            if (banda.Albuns.Count > 0)
+            {
+                Console.Write($"Qual a nota que o álbum {tituloAlbum} merece: ");
+                Avaliacao nota = Avaliacao.Parse(Console.ReadLine()!);
+                Album album = ???
+                album.AdicionarNota(nota);
+                Console.WriteLine($"\nA nota {nota.Nota} foi registrada com sucesso para o álbum {tituloAlbum}");
+                Thread.Sleep(2000);
+                Console.Clear();
+            }
+        }
+        else
+        {
+            Console.WriteLine($"\nA banda {nomeDaBanda} não foi encontrada!");
+            Console.WriteLine("Digite uma tecla para voltar ao menu principal");
+            Console.ReadKey();
+            Console.Clear();
+        }
+    }
+}
