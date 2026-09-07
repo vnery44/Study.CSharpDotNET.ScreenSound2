b7f560742b6e4befc33ce1e06e671bb01ce27574
Vinicius Nery <vinicius_nery2@hotmail.com>
2026-09-07 16:55:45 -0300

    criada a opção do menu de avaliar Album

--- ScreenSound/Menus/MenuAvaliarAlbum.cs
+++ ScreenSound/Menus/MenuAvaliarAlbum.cs
@@ -16,14 +16,20 @@ internal class MenuAvaliarAlbum : Menu
             Banda banda = bandasRegistradas[nomeDaBanda];
             Console.Write("Agora digite o título do álbum: ");
             string tituloAlbum = Console.ReadLine()!;
-            if (banda.Albuns.Count > 0)
+            if (banda.Albuns.Any(a => a.Nome.Equals(tituloAlbum)))
             {
+                Album album = banda.Albuns.First(a => a.Nome.Equals(tituloAlbum));
                 Console.Write($"Qual a nota que o álbum {tituloAlbum} merece: ");
                 Avaliacao nota = Avaliacao.Parse(Console.ReadLine()!);
-                Album album = ???
                 album.AdicionarNota(nota);
                 Console.WriteLine($"\nA nota {nota.Nota} foi registrada com sucesso para o álbum {tituloAlbum}");
                 Thread.Sleep(2000);
+                Console.Clear();
+            } else
+            {
+                Console.WriteLine($"\nO Album {tituloAlbum} não foi encontrado!");
+                Console.WriteLine("Digite uma tecla para voltar ao menu principal");
+                Console.ReadKey();
                 Console.Clear();
             }
         }
